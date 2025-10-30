7e6026582f802f151026c41d1191132a3364f42f
Cristian <cperez90@alumnes.politecnicllevant.cat>
2025-10-30 13:09:43 +0100

    MovieServlet

---
+++ src/main/java/org/daw/firstweb/MovieServlet.java
@@ -0,0 +1,27 @@
+package org.daw.firstweb;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MovieServlet extends HttpServlet {
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+
+        PrintWriter out = resp.getWriter();
+
+        out.println("<h1>Hola som una Movie</h1>");
+    }
+}
